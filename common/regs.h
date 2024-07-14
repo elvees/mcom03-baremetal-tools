51efb2ddde6981b6dfe71931a25671d661dec345
Vasiliy Zasukhin <vzasukhin@elvees.com>
2024-07-14 20:08:36 +0300

    Remove unused typedef from regs.h
    Typedef uart_regs_t is not used. UART driver uses struct that described
    in uart.h.
    
    Change-Id: I1f3159c67a9f4f0d9ab65b574c8483eadbbbe288

--- common/regs.h
+++ common/regs.h
@@ -119,52 +119,6 @@
 #define QSPI1_BASE		  0x10260000
 
 typedef struct {
-	union {
-		volatile uint32_t RBR;
-		volatile uint32_t DLL;
-		volatile uint32_t THR;
-	};
-	union {
-		volatile uint32_t DLH;
-		volatile uint32_t IER;
-	};
-	union {
-		volatile uint32_t FCR;
-		volatile uint32_t IIR;
-	};
-	volatile uint32_t LCR;
-	volatile uint32_t MCR;
-	volatile uint32_t LSR;
-	volatile uint32_t MSR;
-	volatile uint32_t SCR;
-	volatile uint32_t SRBR[16];
-	volatile uint32_t FAR;
-	volatile uint32_t TFR;
-	volatile uint32_t RFW;
-	volatile uint32_t USR;
-	volatile uint32_t TFL;
-	volatile uint32_t RFL;
-	volatile uint32_t SRR;
-	volatile uint32_t SRTS;
-	volatile uint32_t SBCR;
-	volatile uint32_t SDMAM;
-	volatile uint32_t SFE;
-	volatile uint32_t SRT;
-	volatile uint32_t STET;
-	volatile uint32_t HTX;
-	volatile uint32_t DMASA;
-	volatile uint32_t TCR;
-	volatile uint32_t DE_EN;
-	volatile uint32_t RE_EN;
-	volatile uint32_t DET;
-	volatile uint32_t TAT;
-	volatile uint32_t DLF;
-	volatile uint32_t CPR;
-	volatile uint32_t UCV;
-	volatile uint32_t CTR;
-} uart_regs_t;
-
-typedef struct {
 	volatile uint32_t CTR_REG[16];
 	volatile uint32_t BP_CTR_REG;
 	volatile uint32_t SYNC_CLK_REG;
