f87d650c367ae606d4b967d8b898d4c86b08c1a5
Maxim Romanov <mromanov@elvees.com>
2026-03-10 10:54:58 +0300

    gpio: Fix gpio_get_value macro
    Pin value should be read from the different location.
    
    Change-Id: I85c867721fb85361032a01a168282986160991c8

--- common/gpio.h
+++ common/gpio.h
@@ -10,9 +10,9 @@
 #define GPIO1 GPIO1_BASE
 
 #define GPIO_BANK_A 0
-#define GPIO_BANK_B 0xc
-#define GPIO_BANK_C 0x18
-#define GPIO_BANK_D 0x24
+#define GPIO_BANK_B 1
+#define GPIO_BANK_C 2
+#define GPIO_BANK_D 3
 
 #define GPIO_FUNC_GPIO	 0
 #define GPIO_FUNC_PERIPH 1
@@ -20,25 +20,25 @@
 #define GPIO_DIR_IN  0
 #define GPIO_DIR_OUT 1
 
-#define _gpio_set_mask(g, b, offset, mask, val, val_for_zero) \
-	{                                                     \
-		if ((val) == (val_for_zero))                  \
-			REG((g) + (b) + (offset)) &= ~(mask); \
-		else                                          \
-			REG((g) + (b) + (offset)) |= (mask);  \
+#define _gpio_set_mask(g, b, offset, mask, val, val_for_zero)       \
+	{                                                           \
+		if ((val) == (val_for_zero))                        \
+			REG((g) + (b) * 0xc + (offset)) &= ~(mask); \
+		else                                                \
+			REG((g) + (b) * 0xc + (offset)) |= (mask);  \
 	}
 
 #define gpio_set_function_mask(g, b, mask, func) \
 	_gpio_set_mask(g, b, 0x8, mask, func, GPIO_FUNC_GPIO)
 #define gpio_set_direction_mask(g, b, mask, dir) _gpio_set_mask(g, b, 0x4, mask, dir, GPIO_DIR_IN)
-#define gpio_set_value_mask(g, b, mask, val_mask)                                \
-	{                                                                        \
-		REG((g) + (b)) = (REG((g) + (b)) & ~(mask)) | (val_mask & mask); \
+#define gpio_set_value_mask(g, b, mask, val_mask)                                            \
+	{                                                                                    \
+		REG((g) + (b) * 0xc) = (REG((g) + (b) * 0xc) & ~(mask)) | (val_mask & mask); \
 	}
 
 #define gpio_set_function(g, b, pin, func) gpio_set_function_mask(g, b, BIT(pin), func)
 #define gpio_set_direction(g, b, pin, dir) gpio_set_direction_mask(g, b, BIT(pin), dir)
 #define gpio_set_value(g, b, pin, val)	   gpio_set_value_mask(g, b, BIT(pin), (!!(val)) << pin)
-#define gpio_get_value(g, b, pin)	   ((REG((g) + (b)) >> pin) & 0x1)
+#define gpio_get_value(g, b, pin)	   ((REG((g) + 0x50 + (b) * 0x4) >> pin) & 0x1)
 
 #endif
